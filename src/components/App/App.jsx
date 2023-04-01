194a94b82b92b744c0509970e6d97337b49debeb
Oleh Polupan <110285061+Oleh202208@users.noreply.github.com>
2023-04-01 18:09:01 +0200

    finish

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,16 +1,18 @@
+import { ContactList } from 'components/ContactList/ContactList';
+import { Filter } from 'components/Filter/Filter';
+import { ContactsForm } from 'components/Form/ContactForm';
+import styles from './App.module.css';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div className={styles.container}>
+      <h2 className={styles.title}>Phone book</h2>
+      <ContactsForm />
+      <h2 className={styles.title}>Contacts</h2>
+      <div className={styles.contactListContainer}>
+        <Filter />
+        <ContactList />
+      </div>
     </div>
   );
 };
